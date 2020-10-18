50f6a1e758c3362b128613344e1c08a84d96847e
starktrent <jamesstark145@gmail.com>
2020-10-18 23:45:58 +0530

    cockpi & St changes 8h

--- assets/css/custom-colors/custom-colors.js
+++ assets/css/custom-colors/custom-colors.js
@@ -133,127 +133,112 @@ $(".colors-container>div").click(function() {
 
     if (colorID == "color1") {
         $(".brown_v1_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#631e16");
-        $("#color1").css("border", "2px solid #c5c5c5");
+        $("#color1").css("border", "2px solid #000");
     }
 
     if (colorID == "color2") {
         $(".brown_v2_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#782920");
-        $("#color2").css("border", "2px solid #c5c5c5");
+        $("#color2").css("border", "2px solid #000");
     }
 
     if (colorID == "color3") {
         $(".purple_v1_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#522d60");
-        $("#color3").css("border", "2px solid #c5c5c5");
+        $("#color3").css("border", "2px solid #000");
     }
 
     if (colorID == "color4") {
         $(".purple_v2_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#4a235a");
-        $("#color4").css("border", "2px solid #c5c5c5");
+        $("#color4").css("border", "2px solid #000");
     }
 
     if (colorID == "color5") {
         $(".blue_v1_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#164360");
-        $("#color5").css("border", "2px solid #c5c5c5");
+        $("#color5").css("border", "2px solid #000");
     }
 
     if (colorID == "color6") {
         $(".blue_v2_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#1a4f71");
-        $("#color6").css("border", "2px solid #c5c5c5");
+        $("#color6").css("border", "2px solid #000");
     }
 
     if (colorID == "color7") {
         $(".green_v1_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#0e6252");
-        $("#color7").css("border", "2px solid #c5c5c5");
+        $("#color7").css("border", "2px solid #000");
     }
 
     if (colorID == "color8") {
         $(".green_v2_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#0c5346");
-        $("#color8").css("border", "2px solid #c5c5c5");
+        $("#color8").css("border", "2px solid #000");
     }
 
     if (colorID == "color9") {
         $(".green_v3_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#145b33");
-        $("#color9").css("border", "2px solid #c5c5c5");
+        $("#color9").css("border", "2px solid #000");
     }
 
     if (colorID == "color10") {
         $(".green_v4_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#186a3a");
-        $("#color10").css("border", "2px solid #c5c5c5");
+        $("#color10").css("border", "2px solid #000");
     }
 
     if (colorID == "color11") {
         $(".yellow_v1_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#7d6608");
-        $("#color11").css("border", "2px solid #c5c5c5");
+        $("#color11").css("border", "2px solid #000");
     }
 
     if (colorID == "color12") {
         $(".yellow_v2_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#7d5108");
-        $("#color12").css("border", "2px solid #c5c5c5");
+        $("#color12").css("border", "2px solid #000");
     }
 
     if (colorID == "color13") {
         $(".orange_v1_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#784213");
-        $("#color13").css("border", "2px solid #c5c5c5");
+        $("#color13").css("border", "2px solid #000");
     }
 
     if (colorID == "color14") {
         $(".orange_v2_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#6f2c01");
-        $("#color14").css("border", "2px solid #c5c5c5");
+        $("#color14").css("border", "2px solid #000");
     }
 
-    if (colorID == "color15") {
-        $(".gray_v1_100>i").show();
-        $('#color_palette_popup').modal('toggle');
-        $("#color-holder").css('background-color', "#4d5557");
-        $("#color15").css("border", "2px solid #c5c5c5");
-    }
+    // if (colorID == "color15") {
+    //     $(".gray_v1_100>i").show();
+    //     $('#color_palette_popup').modal('toggle');
+    //     $("#color-holder").css('background-color', "#4d5557");
+    //     $("#color15").css("border", "2px solid #000");
+    // }
 
-    if (colorID == "color16") {
-        $(".gray_v2_100>i").show();
-        $('#color_palette_popup').modal('toggle');
-        $("#color-holder").css('background-color', "#424b4a");
-        $("#color16").css("border", "2px solid #c5c5c5");
-    }
+    // if (colorID == "color16") {
+    //     $(".gray_v2_100>i").show();
+    //     $('#color_palette_popup').modal('toggle');
+    //     $("#color-holder").css('background-color', "#424b4a");
+    //     $("#color16").css("border", "2px solid #000");
+    // }
 
-    if (colorID == "color17") {
-        $(".gray_v3_100>i").show();
-        $('#color_palette_popup').modal('toggle');
-        $("#color-holder").css('background-color', "#1c2630");
-        $("#color17").css("border", "2px solid #c5c5c5");
-    }
+    // if (colorID == "color17") {
+    //     $(".gray_v3_100>i").show();
+    //     $('#color_palette_popup').modal('toggle');
+    //     $("#color-holder").css('background-color', "#1c2630");
+    //     $("#color17").css("border", "2px solid #000");
+    // }
 
     if (colorID == "color18") {
         $(".gray_v4_100>i").show();
-        $('#color_palette_popup').modal('toggle');
         $("#color-holder").css('background-color', "#171f2a");
-        $("#color18").css("border", "2px solid #c5c5c5");
+        $("#color18").css("border", "2px solid #000");
     }
  });
